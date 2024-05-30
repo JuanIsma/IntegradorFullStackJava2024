ad5b498dc6f11c4f440a4b213c29c5c68f77cd73
JuanIsma <juanflatron@gmail.com>
2024-05-30 00:51:24 -0300

    Segunda Entrega

--- javaScripts/jscript.js
+++ javaScripts/jscript.js
@@ -1,158 +1,267 @@
-// let slideIndex = 1; // Empezamos en el primer elemento real
-// let isTransitioning = false; // Para evitar que se ejecute la transición múltiples veces
- 
-
-
-// function moveSlide(n) {
-//   if (isTransitioning) return;
-//   isTransitioning = true;
-
-//   const carousel = document.querySelector(".carousel");
-//   const slides = document.querySelectorAll(".carousel-item");
-//   const slideWidth = slides[0].offsetWidth; // Ancho de cada imagen
-
-//   slideIndex += n;
-
-//   carousel.style.transition = "transform 0.5s ease";
-
-//   // Calcular el desplazamiento
-//   const offset = -slideIndex * slideWidth;
-//   carousel.style.transform = `translateX(${offset}px)`;
-
-//   // Cuando termina la transición
-//   carousel.addEventListener("transitionend", () => {
-//     // Si llegamos al final, volver al primer elemento real
-//     if (slideIndex >= slides.length - 1) {
-//       slideIndex = 1;
-//       carousel.style.transition = "none"; // Desactivar la transición para un cambio instantáneo
-//       carousel.style.transform = `translateX(${-slideIndex * slideWidth}px)`;
-//     }
-//     // Si llegamos al principio, volver al último elemento real
-//     else if (slideIndex <= 0) {
-//       slideIndex = slides.length - 2;
-//       carousel.style.transition = "none"; // Desactivar la transición para un cambio instantáneo
-//       carousel.style.transform = `translateX(${-slideIndex * slideWidth}px)`;
-//     }
-
-//     isTransitioning = false; // Permitir que se ejecute otra transición
-//   });
-// }
-
-// // Desplazar automáticamente cada cierto tiempo
-// setInterval(() => {
-//   moveSlide(1);
-// }, 3000); // Cambiar cada 3 segundos (ajustar según sea necesario)
-
-
-
-
-//================================================= API de Películas =========================================================================
-
 const key = "192e0b9821564f26f52949758ea3c473";
-
-// Función para truncar el texto a un máximo de caracteres
-function mostrarTextoHasta(texto, maxCaracteres) {
-  if (texto.length <= maxCaracteres) {
-    return texto;
-  }
-  return texto.substring(0, maxCaracteres) + "...";
-}
-
-//====================================================================
-
+const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZGI2ZmZhZTNlMTUxYjc1ZDQ2NDQxN2M1YTJhZGJmNCIsInN1YiI6IjY2MjQzZmYxN2E5N2FiMDE3ZDhjYzgxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5X3tBw3DNv7kd1Y92t2gSVejwDZorJAxxeMj9sYXR64';
+const baseUrlImagen = 'https://image.tmdb.org/t/p/w500'; // URL base para las imágenes
 
 let pagina = 1;
-const btnAnterior = document.getElementById("btnAnterior");
-const btnSiguiente = document.getElementById("btnSiguiente");
-
-btnSiguiente.addEventListener("click", () => {
-  if (pagina < 1000) {
-    pagina += 1;
-    cargarPeliculasPopulares();
-  }
-});
-
-btnAnterior.addEventListener("click", () => {
-  if (pagina > 1) {
-    pagina -= 1;
-    cargarPeliculasPopulares();
-  }
-});
+ 
+// Función para truncar el texto a un máximo de caracteres
+function mostrarTextoHasta(texto, maxCaracteres) {
+    if (texto.length <= maxCaracteres) {
+        return texto;
+    }
+    return texto.substring(0, maxCaracteres) + "...";
+}
 
 
-//==========================================================================================================================
+//====================================================================================================================
 
-// Cargar películas populares con paginación
-//  <h4 class="titulo2">${pelicula.overview}</h4>
+// Función para cargar películas populares con paginación
 const cargarPeliculasPopulares = async () => {
   try {
-    const respuesta = await fetch(
-      `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=es-MX&page=${pagina}`
-    );
+      const respuesta = await fetch(
+          `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=es-MX&page=${pagina}`
+      );
 
-    if (respuesta.status === 200) {
-      const datos = await respuesta.json();
-      console.log(datos);
-      let peliculas = "";
+      if (respuesta.status === 200) {
+          const datos = await respuesta.json();
+          let peliculas = "";
 
-      datos.results.forEach((pelicula) => {
-        // Convertir la fecha de lanzamiento a un objeto Date
-        let fechaLanzamiento = new Date(pelicula.release_date);
+          datos.results.forEach((pelicula) => {
+              let fechaLanzamiento = new Date(pelicula.release_date);
+              let añoLanzamiento = fechaLanzamiento.getFullYear();
+              const overviewTruncado = mostrarTextoHasta(pelicula.overview, 115);
 
-        // Obtener solo el año
-        let añoLanzamiento = fechaLanzamiento.getFullYear();
+              peliculas += `
+                  <div class="pelicula carusel-item">
+                      <h4 class="titulo2">${añoLanzamiento}</h4><br>
+                      <img class="poster"  src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+                      <h3 class="tituloPelicula">${pelicula.title}</h3>
+                      <h3 class="tituloResumen">${overviewTruncado}</h3>
+                  </div>
+              `;
+          });
 
-        // Truncar el overview a un máximo de 20 caracteres
-        const overviewTruncado = mostrarTextoHasta(pelicula.overview, 115);
-        // Agregar el año al HTML , imagen y resumen
-        peliculas += `
-					<div  class="pelicula carusel-item"  >
-						<h4 class="titulo2">${añoLanzamiento}</h4><br>
-						<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}"   >
-						<h3 class="tituloPelicula">${pelicula.title}</h3>
-						<h3 class="tituloResumen">${overviewTruncado}</h3>
-					</div>
- 
-				`;
-      });
-
-	   //Carga las películas dentro del html
-      document.getElementById("peliculasonline").innerHTML = peliculas;
-    } else if (respuesta.status === 401) {
-      console.log("Pusiste la llave mal");
-    } else if (respuesta.status === 404) {
-      console.log("La película que buscas no existe");
-    } else {
-      console.log("Hubo un error y no sabemos qué pasó");
-    }
+          document.getElementById("peliculasonline").innerHTML = peliculas;
+      } else if (respuesta.status === 401) {
+          console.log("Pusiste la llave mal");
+      } else if (respuesta.status === 404) {
+          console.log("La película que buscas no existe");
+      } else {
+          console.log("Hubo un error y no sabemos qué pasó");
+      }
   } catch (error) {
-    console.log(error);
+      console.log(error);
   }
+};
+
+// Función para cargar la página siguiente de películas populares
+const paginaSiguiente = () => {
+  pagina++;
+  cargarPeliculasPopulares();
+};
+
+// Función para cargar la página anterior de películas populares
+const paginaAnterior = () => {
+  if (pagina > 1) {
+      pagina--;
+      cargarPeliculasPopulares();
+  }
+};
+
+// Asignar los event listeners a los botones de siguiente y anterior
+document.getElementById('botonSiguiente').addEventListener('click', paginaSiguiente);
+document.getElementById('botonAnterior').addEventListener('click', paginaAnterior);
+
+
+
+//====================================================================================================================
+
+
+
+// Función para cargar películas desde la API con una consulta específica
+const cargarPeliculasAPI = async (consulta) => {
+    try {
+        const respuesta = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(consulta)}&include_adult=false&language=es-MX&page=1&api_key=${key}`);
+
+        if (respuesta.status === 200) {
+            const datos = await respuesta.json();
+            mostrarResultados(datos);
+        } else if (respuesta.status === 401) {
+            console.log("Pusiste la llave mal");
+        } else if (respuesta.status === 404) {
+            console.log("La película que buscas no existe");
+        } else {
+            console.log("Hubo un error y no sabemos qué pasó");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// Función para mostrar los resultados de la búsqueda
+function mostrarResultados(data) {
+    const resultadosDiv = document.getElementById('resultados');
+    resultadosDiv.innerHTML = ''; // Limpia los resultados anteriores
+
+    if (data.results && data.results.length > 0) {
+        data.results.forEach(pelicula => {
+            const peliculaDiv = document.createElement('div');
+            peliculaDiv.classList.add('pelicula');
+            peliculaDiv.id = 'peliBuscada'; // Agrega el ID  
+           
+            const imagenSrc = pelicula.poster_path ? `${baseUrlImagen}${pelicula.poster_path}` : 'https://via.placeholder.com/100';
+            const imagen = `<img src="${imagenSrc}" alt="${pelicula.title}" class="movie-poster">`;
+            peliculaDiv.innerHTML = `
+            <div class="movie-container">
+               <article id="margenFotoBuscada">${imagen}</article>
+               <aside> 
+                 <h2 id="tituloPeliBuscada">${pelicula.title}</h2> 
+                 <p id="peliResumen">${pelicula.overview}</p>
+                 <p id="fechaLanzamiento"><strong>Fecha de lanzamiento:</strong> ${pelicula.release_date}</p>
+                 <p id="puntuacion"><strong>Puntuación:</strong> ${pelicula.vote_average}</p>
+                 <button id="botonTrailer" onclick="obtenerTrailer(${pelicula.id})">Ver Tráiler</button>
+               </aside>
+            </div>
+            `;
+
+            resultadosDiv.appendChild(peliculaDiv);
+        });
+    } else {
+        resultadosDiv.innerHTML = '<p>No se encontraron resultados.</p>';
+    }
+
+    // Agrega un evento que escuche los cambios en el input.
+    document.getElementById('input-busqueda').addEventListener('input', function(event) {
+        const query = event.target.value.trim();
+        const resultados = document.getElementById('resultados');
+        const peliculasOnline = document.getElementById('peliculasonline');
+        const textPelicula = document.getElementById('textPelicula');
+        const botonAnterior = document.getElementById('botonAnterior');
+        const botonSiguiente = document.getElementById('botonSiguiente');
+
+        if (query === '') {
+            // Ocultar el contenedor de resultados
+            resultados.style.display = 'none';
+            // Mostrar el contenedor de películas populares
+            peliculasOnline.classList.remove('oculto');
+            textPelicula.classList.remove('oculto');
+            botonAnterior.classList.remove('oculto');
+            botonSiguiente.classList.remove('oculto');
+            // Actualizar la página
+            location.reload();
+        } else {
+            // Mostrar el contenedor de resultados
+            resultados.style.display = 'block';
+            // Ocultar el contenedor de películas populares
+            peliculasOnline.classList.add('oculto');
+            textPelicula.classList.add('oculto');
+            botonAnterior.classList.add('oculto');
+            botonSiguiente.classList.add('oculto');
+        }
+    });
+}
+
+
+
+
+
+
+// Función para obtener y mostrar el tráiler de la película en una ventana modal
+function obtenerTrailer(movieId) {
+    const opciones = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${apiKey}`
+        }
+    };
+
+    fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?language=es-ES`, opciones)
+        .then(response => response.json())
+        .then(data => {
+            const trailer = data.results.find(video => video.type === 'Trailer' && video.site === 'YouTube');
+            if (trailer) {
+                const trailerContainer = document.getElementById('trailer-container');
+                trailerContainer.innerHTML = `<iframe width="880" height="495" src="https://www.youtube.com/embed/${trailer.key}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+                abrirModal();
+            } else {
+                alert('No se encontró tráiler.');
+            }
+        })
+        .catch(err => console.error(err));
+}
+
+
+
+
+// Función para abrir la ventana modal
+function abrirModal() {
+    const modal = document.getElementById('myModal');
+    modal.style.display = 'block';
+}
+
+// Función para cerrar la ventana modal
+function cerrarModal() {
+    const modal = document.getElementById('myModal');
+    modal.style.display = 'none';
+    document.getElementById('trailer-container').innerHTML = '';  // Limpiar el contenido del tráiler
+}
+
+// Obtener el elemento <span> que cierra la ventana modal
+const span = document.getElementsByClassName('close')[0];
+span.onclick = function() {
+    cerrarModal();
+}
+
+// Cuando el usuario hace clic fuera de la ventana modal, se cierra
+window.onclick = function(event) {
+    const modal = document.getElementById('myModal');
+    if (event.target == modal) {
+        cerrarModal();
+    }
 };
 
  
 
-//==========================================================================================================================
  
+
+document.getElementById('buscadorPeliculas').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
+    const consulta = document.getElementById('input-busqueda').value;
+    const resultados = document.getElementById('resultados');
+    const peliculasOnline = document.getElementById('peliculasonline');
+    const textPelicula = document.getElementById('textPelicula');
+    const botonAnterior = document.getElementById('botonAnterior');
+    const botonSiguiente = document.getElementById('botonSiguiente');
+
+    if (consulta && consulta.trim() !== "") {
+        cargarPeliculasAPI(consulta);
+        resultados.style.display = 'block'; // Mostrar los resultados
+        peliculasOnline.classList.add('oculto');
+        textPelicula.classList.add('oculto');
+        botonAnterior.classList.add('oculto');
+        botonSiguiente.classList.add('oculto');
+    } else {
+        // Ocultar el contenedor de resultados si no se ingresa ninguna consulta
+        resultados.style.display = 'none';
+        // Mostrar el contenedor de películas populares si no se ingresa ninguna consulta
+        peliculasOnline.classList.remove('oculto');
+        textPelicula.classList.remove('oculto');
+        botonAnterior.classList.remove('oculto');
+        botonSiguiente.classList.remove('oculto');
+    }
+});
+
 // Inicializa cargando las películas populares
 cargarPeliculasPopulares();
 
 
-//=================================================
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Función para desplazar la página hacia arriba
-  function desplazarArriba() {
-    window.scrollBy(0, -2600); // Ajusta el valor -50 a la cantidad de píxeles que deseas desplazar hacia arriba
-  }
-
-  // Obtener los botones por su ID
-  const btnAnterior = document.getElementById("btnAnterior");
-  const btnSiguiente = document.getElementById("btnSiguiente");
-
-  // Añadir evento de clic a los botones
-  btnAnterior.addEventListener("click", desplazarArriba);
-  btnSiguiente.addEventListener("click", desplazarArriba);
+     
+//Subir pagina hacia arriba al dar un evento click
+document.querySelector('.flechaArriba').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 });
